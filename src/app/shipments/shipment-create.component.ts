078f952ba9ec6d51ff700a3416d97e05a569ba52
Uri Goldshtein <urigoldshtein@Dotans-MacBook-Pro.local>
2016-07-11 18:30:28 +0200

    Add addShipment mutation

--- src/app/shipments/shipment-create.component.ts
+++ src/app/shipments/shipment-create.component.ts
@@ -1,30 +1,60 @@
 import { Component } from '@angular/core';
 import { Shipment } from '../shared/models';
 import { InventoryViewComponent } from '../inventory/inventory-view.component';
-import { ShipmentService } from '../shared/shipment.service';
 import { Router } from '@angular/router';
+import { Apollo } from 'angular2-apollo';
+import gql from 'graphql-tag';
+import { GraphQLResult } from 'graphql';
+import { client } from '../apollo-client-init';
 
 @Component({
-    selector: 'shipment-create',
-    template: `<h2>New Shipment</h2>
-    
-    
+  selector: 'shipment-create',
+  template: `<h2>New Shipment</h2>
+
+
     <div><input [(ngModel)]="shipment.name"></div>
     <div><input [(ngModel)]="shipment.captain"></div>
     <inventory-view [inventory]="shipment.inventory" [editable]="true"></inventory-view>
     <button (click)="save()">Create</button>
-    
+
     `,
-    directives: [InventoryViewComponent],
+  directives: [InventoryViewComponent],
+})
+@Apollo({
+  client,
+  mutations(component: ShipmentCreateComponent) {
+    return {
+      add: () => ({
+        mutation: gql`
+          mutation addShipment(
+            $name: String!
+            $captain: String!
+            $inventory: Product[]!
+          ) {
+            addShipment(
+              name: $name
+              captain: $captain
+              inventory: $inventory
+            ) {
+              id
+            }
+          }
+        `,
+        variables: component.shipment
+      })
+    };
+  }
 })
 export class ShipmentCreateComponent {
-    shipment : Shipment;
+  shipment : Shipment;
+  add: () => Promise<GraphQLResult>;
 
-    constructor(private shipmentService : ShipmentService, private router : Router ) {
-        this.shipment = new Shipment(Math.round(Math.random()*100000));
-    }
-    save() {
-        this.shipmentService.add(this.shipment);
-        this.router.navigateByUrl('/');
-    }
+  constructor(private router : Router ) {
+    this.shipment = new Shipment(Math.round(Math.random()*100000));
+  }
+
+  save() {
+    this.add();
+    this.router.navigateByUrl('/');
+  }
 }
